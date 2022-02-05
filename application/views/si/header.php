18e4170ab87cae97fb1f0498f5a66911ff0132ea
uteknoid <muhrafli55@gmail.com>
2022-02-05 17:49:06 +0800

    Perbaikan oleh UTeknoID 5 Februari 2022

--- application/views/si/header.php
+++ application/views/si/header.php
@@ -3,10 +3,17 @@
 
 <head>
 	<meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title> <?php echo $title; ?> | <?php echo get_store_name(); ?></title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" />    
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<title> <?php echo $title; ?> | <?php echo get_store_name(); ?></title>
+
+	<link rel="stylesheet" href="<?= base_url('assets/themes/interior/css/'); ?>bootstrap.min.css">
+
+	<script src="<?= base_url('assets/themes/interior/js/'); ?>jquery-3.2.1.min.js"></script>
+	<script src="<?= base_url('assets/themes/interior/js/'); ?>popper.min.js"></script>
+	<script src="<?= base_url('assets/themes/interior/js/'); ?>bootstrap.min.js"></script>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" />    
 	<link href="<?= base_url('assets/si/'); ?>css/templatemo-style.css" rel="stylesheet" />
 	<link rel="icon" href="<?php echo base_url('assets/uploads/sites/Logo.png'); ?>">
 </head>
@@ -14,33 +21,47 @@
 <body> 
 
 	<div class="container">
-	<!-- Top box -->
+		<!-- Top box -->
 		<!-- Logo & Site Name -->
 		<div class="placeholder">
 			<div class="parallax-window" data-parallax="scroll" data-image-src="<?= base_url('assets/si/'); ?>img/bgjokk.jpg">
 				<div class="tm-header">
 					<div class="row tm-header-inner">
 						<div class="col-md-6 col-12">
-							<img src="<?= base_url('assets/si/'); ?>img/BI.png" alt="Logo" class="tm-site-logo"/> 
-							<div class="tm-site-text-box">
-								<h6 class="tm-site-description">Comfort With Style</h6>	
-							</div>
+							<img src="<?= base_url('assets/si/'); ?>img/BI.png" alt="Logo" class="tm-site-logo" style="width: 15rem;"/>
 						</div>
 
-					
-								
-								
 						<nav class="col-md-6 col-12 tm-nav">
 							<ul class="tm-nav-ul">
-								<li class="tm-nav-li"><a href="<?= base_url('index.php/index/'); ?>" class="tm-nav-link">Home</a></li>
-								<li class="tm-nav-li"><a href="#" class="tm-nav-link">Galery</a></li>
-								<li class="tm-nav-li"><a href="<?= base_url('index.php/home/'); ?>" class="tm-nav-link">Belanja</a></li>
-								<li class="tm-nav-li"><a href="<?php echo site_url('pages/about'); ?>" class="tm-nav-link">About</a></li>
-								<li class="tm-nav-li"><a href="<?php echo site_url('pages/contact'); ?>" class="tm-nav-link">Kontak</a></li>
-								<li class="tm-nav-li"><a href="#" class="tm-nav-link">Akun</a></li>
+								<li class="tm-nav-li"><a href="<?= base_url('index.php/index/'); ?>" class="tm-nav-link" style="color: #00E4FF; text-decoration: none;">Home</a></li>
+								<li class="tm-nav-li"><a href="#galery" class="tm-nav-link" style="color: #00E4FF; text-decoration: none;">Galery</a></li>
+								<li class="tm-nav-li"><a href="<?= base_url('index.php/home/'); ?>" class="tm-nav-link" style="color: #00E4FF; text-decoration: none;">Belanja</a></li>
+								<li class="tm-nav-li"><a href="<?php echo site_url('pages/about'); ?>" class="tm-nav-link" style="color: #00E4FF; text-decoration: none;">About</a></li>
+								<li class="tm-nav-li"><a href="<?php echo site_url('pages/contact'); ?>" class="tm-nav-link" style="color: #00E4FF; text-decoration: none;">Kontak</a></li>
+								<li class="tm-nav-li dropdown">
+									<a class="tm-nav-link" href="#" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: #00E4FF; text-decoration: none;">Akun</a>
+									<div class="dropdown-menu" aria-labelledby="dropdown05">
+										<?php if ( is_login() && is_customer()) : ?>
+										<a class="dropdown-item" href="<?php echo site_url('customer'); ?>">Akun saya</a>
+										<a class="dropdown-item" href="<?php echo site_url('customer/orders'); ?>">Order</a>
+										<div class="divider"></div>
+										<a class="dropdown-item" href="<?php echo site_url('auth/logout'); ?>">Logout</a>
+										<?php elseif ( is_login() && is_admin()) : ?>
+										<a class="dropdown-item" href="<?php echo site_url('admin'); ?>">Dasbor</a>
+										<div class="divider"></div>
+										<a class="dropdown-item" href="<?php echo site_url('auth/logout'); ?>">Logout</a>
+										<?php else : ?>
+											<a class="dropdown-item" href="<?php echo site_url('auth/login'); ?>">Masuk Log</a>
+											<a class="dropdown-item" href="<?php echo site_url('auth/register'); ?>">Daftar</a>
+										<?php endif; ?>
+									</div>
+								</li>
 
 							</ul>
+
 						</nav>	
+
+
 					</div>
 				</div>
 			</div>
--- application/views/si/informasi.php
+++ application/views/si/informasi.php
@@ -1,21 +1,33 @@
+		<style type="text/css">
+			.tm-gallery-item{
+				height: 30rem;
+			}
+			.galeri-desain{
+				height: 29rem;
+			}
+			.galeri-desain:hover{
+				box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+			}
+		</style>
+
 		<main>
 			<header class="row tm-welcome-section">
 				<h2 class="col-12 text-center tm-section-title">Sistem Informasi Penjualan Desain Interior Mobil</h2>
-				 <p  class="col-12 text-center" style="color: #000;"> Interior Mobil dengan desain mewah & nyaman tentunya dapat menjadi idaman banyak orang. Bahkan setiap orang mampu memiliki standar yang berbeda satu sama lain yang berkaitan dengan kenyamanan dalam berkendara. Sebagian besar orang ada yang merasa nyaman dengan sebuah desain interior yang mewah bahkan ada juga yang telah nyaman dengan desain interior bawaan dari mobil tersebut.</p>
+				<p  class="col-12 text-center" style="color: #000;"> Interior Mobil dengan desain mewah & nyaman tentunya dapat menjadi idaman banyak orang. Bahkan setiap orang mampu memiliki standar yang berbeda satu sama lain yang berkaitan dengan kenyamanan dalam berkendara. Sebagian besar orang ada yang merasa nyaman dengan sebuah desain interior yang mewah bahkan ada juga yang telah nyaman dengan desain interior bawaan dari mobil tersebut.</p>
 				<p class="col-12 text-center" style="color: #000;"> Salah satu bagian interior yang juga cukup sering menjadi salah satu perhatian ketika ingin memodifikasi mobil yaitu Jok Mobil. Bukan hanya karena ingin interior mobil terlihat mewah, tetapi juga agar jok mobil tidak mudah kotor dan jamuran pada saat ditumpahi air dan kotoran lainnya. Diharapkan pemasangan jok mobil bisa membuat interior mobil nampak lebih mewah dan menarik namun juga terhindar dari resiko kotoran dan jamur.</p>
 			</header>
 			
-			<div class="tm-paging-links">
+			<div class="tm-paging-links" id="galery">
 				<nav>
 					<ul>
-						<li class="tm-paging-item"><a href="#galery" class="tm-paging-link active">Galery</a></li>
-						<li class="tm-paging-item"><a href="#galery" class="tm-paging-link">Bahan</a></li>
+						<li class="tm-paging-item"><a href="#galery" class="tm-paging-link active" style="text-decoration: none;">Galery</a></li>
+						<li class="tm-paging-item"><a href="#galery" class="tm-paging-link" style="text-decoration: none;">Bahan</a></li>
 					</ul>
 				</nav>
 			</div>
 
 			<!-- Gallery -->
-			<div class="row tm-gallery" id="galery">
+			<div class="row tm-gallery">
 				<!-- gallery page 1 -->
 				<div id="tm-gallery-page-galery" class="tm-gallery-page">
 
@@ -24,15 +36,40 @@
 					foreach ($galery as $g) {
 						?>
 
-						<article class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
-							<figure>
-								<img src="<?php echo base_url('assets/uploads/informasi/galery/'. $g->gambar); ?>" alt="Image" class="img-fluid tm-gallery-img" />
-								<figcaption>
-									<h4 class="tm-gallery-title"><?php echo $g->nama; ?></h4>
-									<p class="tm-gallery-description"><?php echo $g->deskripsi; ?></p>
-								</figcaption>
-							</figure>
-						</article>
+						<a href="#" data-toggle="modal" data-target=".bd-example-modal-lg<?php echo $g->id; ?>" style="text-decoration: none;">
+
+							<article class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
+								<figure class="galeri-desain">
+									<img src="<?php echo base_url('assets/uploads/informasi/galery/'. $g->gambar); ?>" alt="Image" class="img-fluid tm-gallery-img" />
+									<figcaption style="padding: 0 15px;">
+										<h4 class="tm-gallery-title" style="text-align: center;"><?php echo $g->nama; ?></h4>
+										<p class="tm-gallery-description" style="text-align: justify;"><?php echo mb_strimwidth("$g->deskripsi", 0, 150, "..."); ?></p>
+									</figcaption>
+								</figure>
+							</article>
+
+						</a>
+
+						<div class="modal fade bd-example-modal-lg<?php echo $g->id; ?>" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+							<div class="modal-dialog modal-lg">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="exampleModalLabel"><?php echo $g->nama; ?></h5>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+										</button>
+									</div>
+									<div class="modal-body" style="padding: 0 15px;">
+										<img src="<?php echo base_url('assets/uploads/informasi/galery/'. $g->gambar); ?>" alt="Image" class="img-fluid text-center mb-3 mt-3" style="width: 100%;">
+										<figcaption>
+											<p class="tm-gallery-description" style="text-align: justify; font-size: 1em;">
+												<?php echo $g->deskripsi; ?>
+											</p>
+										</figcaption>
+									</div>
+								</div>
+							</div>
+						</div>
 
 					<?php } ?>
 
