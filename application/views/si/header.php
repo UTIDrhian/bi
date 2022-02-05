a435f54e01adadba251809e462529bb33a071588
UTIDrhian <rhiansaeputra22@gmail.com>
2022-02-05 12:20:38 +0700

    ganti header

--- application/views/si/header.php
+++ application/views/si/header.php
@@ -5,9 +5,10 @@
 	<meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title><?php echo $title; ?></title>
+    <title> <?php echo $title; ?> | <?php echo get_store_name(); ?></title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" />    
 	<link href="<?= base_url('assets/si/'); ?>css/templatemo-style.css" rel="stylesheet" />
+	<link rel="icon" href="<?php echo base_url('assets/uploads/sites/Logo.png'); ?>">
 </head>
 
 <body> 
